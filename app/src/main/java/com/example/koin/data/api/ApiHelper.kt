85154d0ff489c02850feaddddc2436e58b171e10
Ashu Prasad <aashuprasad@live.com>
2023-03-24 18:07:33 +0530

    final commit

--- app/src/main/java/com/example/koin/data/api/ApiHelper.kt
+++ app/src/main/java/com/example/koin/data/api/ApiHelper.kt
@@ -3,7 +3,6 @@ package com.example.koin.data.api
 import com.example.koin.data.model.User
 import retrofit2.Response
 
-@Singleton
 interface ApiHelper {
     suspend fun getUsers(): Response<List<User>>
 }
